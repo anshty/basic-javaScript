868e13bcce776d8199616b94bd838b1ec4f42829
anshty <anshverma90541@gmail.com>
2024-02-12 23:50:22 +0530

    datatypesummary

---
+++ datatypesummary.js
@@ -0,0 +1,17 @@
+//primitive datatype
+
+//7 types=> String,number,Boolean,Null,undefined,Symbol,BigInt
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+console.log(id ===anotherid);//false
+
+//const bigNumber = BigInt('111111111111111111111111111111111111111111111111111111111111111111')
+const bigNumber=123456789098765431234567890n
+console.log(bigNumber);
+
+
+//reference(non primitive)
+
+//Array,Objects ,Funtions
