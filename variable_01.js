acff6b5e91dbff6887f7333e947f96df7990c7ae
anshty <anshverma90541@gmail.com>
2024-02-12 12:39:23 +0530

    variable_01topic

---
+++ variable_01.js
@@ -0,0 +1,25 @@
+const accountId = 144553 //id locked one's its declare
+let accountEmail="ankitverma@gmail.com"
+
+var accountpassword="1234"
+
+accountcity="bhilai"
+//accountId=2 //not allowed
+console.log(accountId);
+
+accountEmail="av10@gmail.com"
+accountpassword= '11092'
+accountcity='raipur' 
+
+let accountstate;
+
+/*
+
+prefer not to use var 
+because of issue in block scope and funtional scope
+
+
+*/
+console.table([accountEmail,accountId,accountpassword,accountcity,accountstate])
+
+
