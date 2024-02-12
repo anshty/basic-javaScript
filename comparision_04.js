9467605945de90ebbef4eed94fec8fe26b8e08c6
anshty <anshverma90541@gmail.com>
2024-02-12 23:25:09 +0530

    comparision_04

---
+++ comparision_04.js
@@ -0,0 +1,32 @@
+console.log(1==2);//equalto
+console.log(1<=2);//smaller then equal
+console.log(1>=2);//greater then equal 
+console.log(1!=2);//not equal to
+console.log(1<2);//smaller then 2
+console.log(1>2);//bigger then 2
+
+console.log("2">1);
+console.log("02">10);
+console.log("null value comparision");
+console.log(null>1);//false
+console.log(null<1);//true
+console.log(null==0);//false
+console.log(null>=0);//true
+console.log(null>0);//false
+console.log(null<0);//false
+console.log(null!=0);//true
+console.log(undefined >null);//false
+console.log(undefined<null);//false
+console.log(undefined >=null);//false
+console.log(undefined <=null);//false
+
+/*
+equality check == and comparisons(convert null into number) >< >= <= work defertly
+*/
+
+console.log(true > 0);//true
+console.log(true == 1);//true
+
+//=== strict check
+
+console.log("2"===2);//false
