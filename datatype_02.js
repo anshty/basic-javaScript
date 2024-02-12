76182504c4ef46f1a494a2d37965fa812e39ba54
anshty <anshverma90541@gmail.com>
2024-02-12 13:31:07 +0530

    datatype_02

---
+++ datatype_02.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as new version
+// alert(3+3) //we are using nodejs ,not browser 
+console.log(3
+    +3)//codereadability should be high
+    // console.log(123)console.log('abc') //this will create error 
+
+    console.log('ankit');
+    let name ="ankit"
+    let age =20
+
+    let islogIn=false
+    let state=null
+    let stateWeather;
+
+
+    // number => 2 to power 53 is range of memory address or storage
+    // BigInt
+    //string=> "" or''
+    // Boolean=>true/false
+    //null => standalone value
+    // undefined
+    //symbol => to define unique 
+    //object
+
+    console.log(typeof 'ankit');//string
+    
+    console.log(typeof null); //object
+    console.log(typeof undefined);//undefined
