d838e8f3009c0e800a14a69aa59e7ea68fb39d09
anshty <anshverma90541@gmail.com>
2024-02-12 14:43:54 +0530

    conversion_03

---
+++ 03_conversionOpration.js
@@ -0,0 +1,50 @@
+// conversion
+let score = 123
+console.log(typeof score); //number{or to use console.log(typeof(score));}
+
+score = "123"
+console.log(typeof score);//string
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber);//number
+
+
+score = '123abc'
+let valueInnumber1 = Number(score)
+console.log(typeof valueInnumber1);//it number but the input is 123abc 
+console.log(valueInnumber1);//value is NaN (not a number)
+
+score=null
+let valueInnumber2 = Number(score)
+console.log('null');
+console.log(valueInnumber2);// value is 0 null in number is 0 
+console.log(typeof valueInnumber2);//it shows number
+
+
+sore = undefined;
+let valueInnumber3 = Number(sore)
+console.log('undefined');
+console.log(valueInnumber3);//here NaN  
+console.log(typeof valueInnumber3);//number bcz value must be in number form
+
+/*   
+ "33"=> 33
+ "33abc"=> NaN
+ true =>1 ; false=>0
+
+*/
+
+
+
+let isloggedIn =1
+
+let booleanisLoggedin =Boolean(isloggedIn)
+console.log(booleanisLoggedin);//true
+console.log(typeof booleanisLoggedin);//boolean
+
+/*
+1=>true ; 0=>false
+""=>false ; "ankit"=>true
+
+*/
+//this is how conversion work it convert value of vareable into deferent data type
+
