f9598616a0506700d390a2c31d4db738dde124a0
anshty <anshverma90541@gmail.com>
2024-02-14 21:49:30 +0530

    num and math 06

---
+++ num_and_math_06.js
@@ -0,0 +1,34 @@
+// const amount=new Number (20)
+
+// console.log(typeof amount);
+// console.log(amount);
+// console.log(amount.toString().length);
+// console.log(amount.toFixed(2));
+
+// const otheramount=233
+// console.log(otheramount.toPrecision(1));//2e+2
+
+// const hundred=10000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+//+++++++++++++++++++++math++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-2));// -2 => 2 & +2 => +2
+// console.log(Math.round(4.3));//make round off
+// console.log(Math.ceil(4.2));//make biger number
+// console.log(Math.floor(4.9));//4
+// console.log(Math.sqrt(625));//25 
+// console.log(Math.min(2,3,5,8,0));
+// console.log(Math.max(0,1,3,4));
+
+console.log(Math.random());//bt 0-1
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+min=1
+max=6
+console.log(Math.floor(Math.random()*(max-min)+1)+min);
