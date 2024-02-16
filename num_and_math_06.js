a08bc69a967cbb6e62b6e21336210d380b7f73d4
anshty <anshverma90541@gmail.com>
2024-02-16 23:23:30 +0530

    updated num and math 06

--- num_and_math_06.js
+++ num_and_math_06.js
@@ -18,7 +18,7 @@
 // console.log(Math);
 // console.log(Math.abs(-2));// -2 => 2 & +2 => +2
 // console.log(Math.round(4.3));//make round off
-// console.log(Math.ceil(4.2));//make biger number
+// console.log(Math.ceil(4.2));//make biger number 5
 // console.log(Math.floor(4.9));//4
 // console.log(Math.sqrt(625));//25 
 // console.log(Math.min(2,3,5,8,0));
