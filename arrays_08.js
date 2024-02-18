e559c42d6d3c815b575d8208b27a5388a356429b
anshty <anshverma90541@gmail.com>
2024-02-19 00:57:37 +0530

    array push data

---
+++ arrays_08.js
@@ -0,0 +1,15 @@
+/*array  The Array object, as with arrays in other programming languages, enables storing a collection of 
+multiple items under a single variable name, and has members for performing common array operations */
+
+const myArry=[0,1,2,3,5];
+console.log(myArry[4]);
+
+myHero=['ironman','spiderman','hulk','thor']
+console.log(myHero);
+console.log(myHero.length);
+myHero.push('deadpool')
+console.log(myHero)
+
+myHero.reduce('ironman')
+
+
