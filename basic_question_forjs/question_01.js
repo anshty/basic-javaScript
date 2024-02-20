4384f846cd03bda71a5d2cb27da96612d7e060aa
anshty <anshverma90541@gmail.com>
2024-02-21 00:07:44 +0530

    updated  local string state

--- basic_question_forjs/question_01.js
+++ basic_question_forjs/question_01.js
@@ -3,7 +3,7 @@ Sample Output : Today is : Tuesday.
  Current time is : 10  : 30 : 38 pm */
 
  let currentDay_Time= new Date()
- console.log(`Today is : ${currentDay_Time.toLocaleString('en-us',{weekday:'long'})}`);
+ console.log(`Today is : ${currentDay_Time.toLocaleString('en-in',{weekday:'long'})}`);
 console.log(new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric',second:'numeric' })); 
 //  console.log(`Current time is :${currentDay_Time.getHours()}:${currentDay_Time.getMinutes()}:${currentDay_Time.getSeconds()}`);
 
