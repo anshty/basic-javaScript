2cde4c03e2eea07a07316f498df860ad2d3b182b
anshty <anshverma90541@gmail.com>
2024-03-01 20:42:44 +0530

    singleton object

--- objecton_.js
+++ objecton_.js
@@ -1,16 +1,68 @@
 const class_id01=Object({
-    name:'Ankit verma',
-    class:12,
-    Section:'bioIp'
+    nae:'Ankit verma',
+    lss:12,
+    ectin:'bioIp',
+    info:{
+        contect:{
+            phoneno:72727378833,
+            mobile:938384822,
+            telecom:{
+                telegram:{
+                    tnum:299492929
+                }
+            }
+        }
+    }
+
 })
 
 const class_id02={
-    name:'ayush verma',
-    class:12,
-    Section:'bioPe'
+    ame:'ayush verma',
+    lass:12,
+    ection:'bioPe'
 }
 
-console.log(class_id01.__prototype__);
-console.log( class_id02.__prototype__);
+const class_id03=new Object()
+class_id03.nam='ankitverma',
+class_id03.clas=12,
+class_id03.Secton='maths'
+
+const class_id04={}
+class_id04.name='ansh verma',
+class_id04.class=12,
+class_id04.Section='maths'
+
+
+// const all_class={...class_id01,...class_id02,...class_id03,...class_id04}
+
+// console.log(all_class);
+
+// const obj=Object.assign({},class_id01,class_id02)
+// console.log(obj);
+
+const project01=[{a:1,b:2},{name:'anku',email_id:'anku@gmail.com'},]
+
+
+// console.log(project01[1].name);
+
+
+console.log(Object.keys(class_id01));
+console.log(Object.values(class_id01));
+
+console.log(class_id01.hasOwnProperty('nae'));
+
+
+
+
+
+
+// console.log(class_id01.info.contect.telecom.telegram);
+// console.log(class_id01);
+// console.log( class_id02,'\n');
+// console.log( class_id03,'\n');
+// console.log( class_id04,'\n');
+
+
+
 
 
