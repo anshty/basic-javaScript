8402c98b25551434204e4bdc1f6491841c72ae1b
anshty <anshverma90541@gmail.com>
2024-03-05 23:14:44 +0530

    while and do while loop

---
+++ basic_function/iteration_loop_03/while_dowhile_02.js
@@ -0,0 +1,26 @@
+let index=1;
+while (index<=10) {
+    // console.log('12'+'*'+index+'='+index*12);
+
+    index++
+}
+
+let myarray=[1,2,3,4,5]
+let arr=0
+while (arr<=myarray.length) {
+
+    console.log(myarray[arr]);
+
+    arr++
+    
+}
+
+
+// do while
+
+let score =11
+
+do{
+    console.log(`score is ${score}`);
+   score++
+}while(score<=10)
