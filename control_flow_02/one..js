bce6b983736d88e1495b7646260e03d2e311e857
anshty <anshverma90541@gmail.com>
2024-03-10 00:14:16 +0530

    html code for dom

---
+++ control_flow_02/one..js
@@ -0,0 +1,38 @@
+// if
+
+// if (2==='2') {
+//     console.log('yes');    
+// }
+
+// if (2!=='2') {
+//     console.log('yupe');
+    
+// }
+num=60
+
+// if(num<50){
+//     console.log('yes');
+// }else{
+//     console.log('no');
+// }
+
+// +++++++shorthand notation
+
+if(num<=99)console.log('true');
+
+// ++++++nasted conditional statment
+
+// if(num<30){
+//     console.log('is less then 30');
+// }else if(num<50||70){
+//     console.log('is between 70 & 50');
+// }else{
+//     console.log('is out ');
+// }
+
+const userloggedIn =true
+const dabitCard=true
+
+if(userloggedIn && dabitCard){
+    console.log(' welcome to our site');
+}
